47000236c3babf5115556590097ec039536b9e7f
sathishSmart <sathishprs@smartglossa.com>
2017-05-30 14:24:11 +0530

    change class file

--- app/src/main/java/smartglossa/com/cal/MainActivity.java
+++ app/src/main/java/smartglossa/com/cal/MainActivity.java
@@ -2,12 +2,35 @@ package smartglossa.com.cal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText res1;
+    EditText res2;
+    TextView res3;
+    Button Add;
+    Integer x,y,z;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        res1=(EditText)findViewById(R.id.edit1);
+        res2=(EditText)findViewById(R.id.edit2);
+        res3=(TextView)findViewById(R.id.res);
+        Add=(Button)findViewById(R.id.button);
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                x=Integer.parseInt(res1.getText().toString());
+                y=Integer.parseInt(res2.getText().toString());
+                z=x+y;
+                res3.setText(Integer.toString(z));
+            }
+        });
+
     }
 }
